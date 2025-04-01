926a8d82f8cb4cfb40c881c1cd7d5ea4e2e0b747
mehdi himmiche <mehdi.himmiche@etu.univ-tlse2.fr>
2025-04-01 12:36:02 +0200

    add front

---
+++ interfac/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-[]">
+      <Form/>
+    </div>
+  );
+}
+
+export default App;
---
+++ interfac/src/modules/Form/index.js
@@ -0,0 +1,8 @@
+const Form = () => {
+    return (
+        <div className="bg-white w-[600px] h-[800px] shadow-lg">
+        </div>
+    )
+}
+
+export default Form
